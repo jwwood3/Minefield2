7fc53f5b12f87b825fc395fc61968f47ae8ac5c3
jwwood3 <60763121+jwwood3@users.noreply.github.com>
2020-08-19 16:21:04 -0700

    Added buffer to prevent items from being autograbbed

--- Assets/Scripts/PopupsScript.cs
+++ Assets/Scripts/PopupsScript.cs
@@ -5,19 +5,42 @@ using UnityEngine;
 public class PopupsScript : MonoBehaviour {
 
     public float life = 3.0f;
+    float fullLife;
+    float timer = 0.4f;
+    Color trueColor;
+    SpriteRenderer sr;
+    Collider2D c2D;
 	// Use this for initialization
 	void Start () {
 		float x = (float)((Random.value*5)-2.5);
 		float y= (float)((Random.value*6)-5);
-		this.gameObject.transform.position=new Vector3(x,y,1);
+        c2D = this.gameObject.GetComponent<Collider2D>();
+        sr = this.gameObject.GetComponent<SpriteRenderer>();
+        trueColor = sr.color;
+        sr.color = Color.yellow;
+        c2D.enabled = false;
+        this.gameObject.transform.position=new Vector3(x,y,1);
+        fullLife = life;
 	}
 
     void FixedUpdate()
     {
-        life -= Time.fixedDeltaTime;
-        if (life <= 0)
+        if (timer > 0)
         {
-            Destroy(this.gameObject);
+            timer -= Time.fixedDeltaTime;
+        }
+        else
+        {
+            if(life == fullLife)
+            {
+                c2D.enabled = true;
+                sr.color = trueColor;
+            }
+            life -= Time.fixedDeltaTime;
+            if (life <= 0)
+            {
+                Destroy(this.gameObject);
+            }
         }
     }
 	
